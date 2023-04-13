b6c910e81891a98e8ab040d382d5852b7b0d9564
Camille Arce <thecamillearce@gmail.com>
2023-04-13 04:23:45 +0000

    finished assignment

--- loops_fizz_buzz.rb
+++ loops_fizz_buzz.rb
@@ -4,3 +4,16 @@
 # for multiples of three print "Fizz" instead of the number
 #   for multiples of five print "Buzz" instead of the number
 #   for numbers which are multiples of both three and five print "FizzBuzz"
+
+100.times do |i|
+  i = (i + 1)
+  if i % 3 == 0 && i % 5 == 0
+    puts "FizzBuzz"
+  elsif i % 3 == 0
+    puts "Fizz"
+  elsif i % 5 == 0
+    puts "Buzz"
+  else
+    puts i
+  end
+end
--- loops_letter_count.rb
+++ loops_letter_count.rb
@@ -1,9 +1,9 @@
 # Write a program that:
-# 
+#
 # Asks the user to input a word and counts from 1 to however long the word is.
-# 
+#
 # Example (`apple` is the input):
-# 
+#
 # "Enter a word:"
 # apple
 # 1
@@ -14,3 +14,15 @@
 # "apple is 5 letters long!"
 
 p "Enter a word:"
+word = gets.chomp
+word_length = word.length
+
+1.upto(word_length) do |i|
+  p i
+end
+
+word_length.times do |i|
+  p i + 1
+end
+
+puts "#{word} is #{word_length} letters long!"
